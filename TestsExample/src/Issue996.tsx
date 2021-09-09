ab9545ca9e1c2941d2db9232c4f47bd99d1b93b4
Ubax <jakub.tkacz@swmansion.com>
2021-09-09 14:20:56 +0300

    test: Adds issue 996

---
+++ TestsExample/src/Issue996.tsx
@@ -0,0 +1,61 @@
+import * as React from 'react';
+import {Button, View} from 'react-native';
+import {NavigationContainer, ParamListBase} from '@react-navigation/native';
+import {
+  createNativeStackNavigator,
+  NativeStackNavigationProp,
+} from 'react-native-screens/native-stack';
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <View style={{flex: 1}}>
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={{gestureEnabled: true}}>
+          <Stack.Screen
+            name="First"
+            component={First}
+            options={{
+              searchBar: {},
+            }}
+          />
+          <Stack.Screen
+            name="Second"
+            component={Second}
+            options={{
+              headerShown: false,
+            }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </View>
+  );
+}
+
+function First({
+  navigation,
+}: {
+  navigation: NativeStackNavigationProp<ParamListBase>;
+}) {
+  return (
+    <View style={{backgroundColor: '#000', flex: 1, justifyContent: 'center'}}>
+      <Button
+        title="Tap me for second screen"
+        onPress={() => navigation.push('Second')}
+      />
+    </View>
+  );
+}
+
+function Second({
+  navigation,
+}: {
+  navigation: NativeStackNavigationProp<ParamListBase>;
+}) {
+  return (
+    <View style={{backgroundColor: '#333', flex: 1, justifyContent: 'center'}}>
+      <Button title="Tap me to go back" onPress={() => navigation.pop()} />
+    </View>
+  );
+}
